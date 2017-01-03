205876b4fbe893baabc9dd77e1ef473a11cd4c57
Mohd Nadeem <mohd.nadeem3464@gmail.com>
2017-01-03 13:12:25 -0800

    Temporal Demo Push

---
+++ TemporalDemo/src/main/java/com/binovizer/data/Application.java
@@ -0,0 +1,38 @@
+package com.binovizer.data;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.binovizer.data.entities.User;
+
+public class Application {
+	public static void main(String[] args) {
+		SessionFactory sf = HibernateUtils.getSessionFactory();
+		Session session = sf.openSession();
+		session.beginTransaction();
+		
+		User nad = new User();
+		nad.setFirstName("Mohd");
+		nad.setLastName("Aayan");
+		nad.setBirthDate(new Date());
+		nad.setEmailAddress("mohd.aayan3464@gmail.com");
+		nad.setLastUpdatedBy("Mohd Nadeem");
+		nad.setLastUpdatedDate(new Date());
+		nad.setCreatedBy("Mohd Nadeem");
+		nad.setCreatedDate(new Date());
+		
+		session.save(nad);
+		session.getTransaction().commit();
+		/*
+		session.beginTransaction();
+		User dbUser = (User) session.get(User.class, nad.getUserId());
+		dbUser.setLastName("Ayan");
+		session.update(dbUser);
+		session.getTransaction().commit();*/
+		
+		session.close();
+		sf.close();
+	}
+}
